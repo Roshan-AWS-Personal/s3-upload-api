e13f6db82ef83843991da16f04c4b16546cf4cba
roshan-venkatesan-9563 <roshanvenkatesan9563@gmail.com>
2025-06-29 11:40:18 +1000

    fixed output value for api

--- live/dev/api_gateway.tf
+++ live/dev/api_gateway.tf
@@ -48,5 +48,5 @@ resource "aws_api_gateway_stage" "stage" {
 }
 
 output "upload_api_url" {
-  value = "${aws_api_gateway_deployment.deployment.invoke_url}/upload"
+  value = "https://${aws_api_gateway_rest_api.upload_api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.stage.stage_name}/upload"
 }
