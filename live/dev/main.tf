e51f39cbbe56e84e78ce884fc9623c76de5bcd58
roshan-venkatesan-9563 <roshanvenkatesan9563@gmail.com>
2025-06-29 10:10:52 +1000

    added lambda terraform blocks

--- live/dev/main.tf
+++ live/dev/main.tf
@@ -51,3 +51,15 @@ resource "aws_iam_role_policy" "lambda_s3_policy" {
     ]
   })
 }
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda"
+  output_path = "${path.module}/lambda.zip"
+}
+
+resource "aws_lambda_function" "image_uploader" {
+  function_name = "image-uploader-dev"
+  filename      = data.archive_file.lambda_zip.output_path
+  role          = aws_iam_role.lambda_exec_role
+}
