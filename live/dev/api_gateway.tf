3a42e6d9256e7ec327861bb9b0b0c5531b7be7b6
roshan-venkatesan-9563 <roshanvenkatesan9563@gmail.com>
2025-07-01 19:16:21 +1000

    presigned url get

--- live/dev/api_gateway.tf
+++ live/dev/api_gateway.tf
@@ -10,17 +10,18 @@ resource "aws_api_gateway_resource" "upload" {
   path_part   = "upload"
 }
 
-resource "aws_api_gateway_method" "post_upload" {
+# GET method (used to get presigned URL)
+resource "aws_api_gateway_method" "get_upload_url_method" {
   rest_api_id   = aws_api_gateway_rest_api.upload_api.id
   resource_id   = aws_api_gateway_resource.upload.id
-  http_method   = "POST"
+  http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id = aws_api_gateway_rest_api.upload_api.id
   resource_id = aws_api_gateway_resource.upload.id
-  http_method = aws_api_gateway_method.post_upload.http_method
+  http_method = aws_api_gateway_method.get_upload_url_method.http_method
 
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -35,18 +36,43 @@ resource "aws_lambda_permission" "api_gateway" {
   source_arn    = "${aws_api_gateway_rest_api.upload_api.execution_arn}/*/*"
 }
 
-resource "aws_api_gateway_deployment" "deployment" {
+# CORS for GET
+resource "aws_api_gateway_method_response" "upload_get_response" {
   rest_api_id = aws_api_gateway_rest_api.upload_api.id
+  resource_id = aws_api_gateway_resource.upload.id
+  http_method = aws_api_gateway_method.get_upload_url_method.http_method
+  status_code = "200"
 
-  depends_on = [aws_api_gateway_integration.lambda_integration]
+  response_models = {
+    "application/json" = "Empty"
+  }
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+  }
 }
 
-resource "aws_api_gateway_stage" "stage" {
-  stage_name    = "dev"
-  rest_api_id   = aws_api_gateway_rest_api.upload_api.id
-  deployment_id = aws_api_gateway_deployment.deployment.id
+resource "aws_api_gateway_integration_response" "upload_get_response" {
+  rest_api_id = aws_api_gateway_rest_api.upload_api.id
+  resource_id = aws_api_gateway_resource.upload.id
+  http_method = aws_api_gateway_method.get_upload_url_method.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
+    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET'"
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_method_response.upload_get_response
+  ]
 }
 
+# OPTIONS method for CORS preflight
 resource "aws_api_gateway_method" "upload_options" {
   rest_api_id   = aws_api_gateway_rest_api.upload_api.id
   resource_id   = aws_api_gateway_resource.upload.id
@@ -99,9 +125,25 @@ resource "aws_api_gateway_integration_response" "upload_options_response" {
   }
 
   depends_on = [
-  aws_api_gateway_integration.upload_options_mock,
-  aws_api_gateway_method_response.upload_options_response
-]
+    aws_api_gateway_integration.upload_options_mock,
+    aws_api_gateway_method_response.upload_options_response
+  ]
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  rest_api_id = aws_api_gateway_rest_api.upload_api.id
+
+  depends_on = [
+    aws_api_gateway_integration.lambda_integration,
+    aws_api_gateway_integration_response.upload_get_response,
+    aws_api_gateway_integration_response.upload_options_response
+  ]
+}
+
+resource "aws_api_gateway_stage" "stage" {
+  stage_name    = "dev"
+  rest_api_id   = aws_api_gateway_rest_api.upload_api.id
+  deployment_id = aws_api_gateway_deployment.deployment.id
 }
 
 output "upload_api_url" {
