d752e42e6e1e96ab334ab675c4fc0f8121a732c4
roshan-venkatesan-9563 <roshanvenkatesan9563@gmail.com>
2025-07-08 22:39:28 +1000

    added depends on for deletion

--- live/prod/api_gateway.tf
+++ live/prod/api_gateway.tf
@@ -134,6 +134,9 @@ resource "aws_api_gateway_stage" "stage" {
   stage_name    = var.stage_name
   rest_api_id   = aws_api_gateway_rest_api.upload_api.id
   deployment_id = aws_api_gateway_deployment.api_deployment.id
+    depends_on = [
+    aws_api_gateway_deployment.api_deployment
+  ]
 }
 
 resource "aws_api_gateway_deployment" "api_deployment" {
