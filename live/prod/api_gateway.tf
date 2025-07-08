9a97201ef9eb945bd97dc8cc60edf78d23711988
roshan-venkatesan-9563 <roshanvenkatesan9563@gmail.com>
2025-07-08 22:33:19 +1000

    fixed upload

--- live/prod/api_gateway.tf
+++ live/prod/api_gateway.tf
@@ -141,7 +141,7 @@ resource "aws_api_gateway_deployment" "api_deployment" {
 
   triggers = {
     redeploy = sha1(jsonencode([
-      aws_api_gateway_resource.upload_api.id,
+      aws_api_gateway_resource.upload.id,
       aws_api_gateway_method.get_upload_url_method.id,
       aws_api_gateway_integration.lambda_integration.id
     ]))
