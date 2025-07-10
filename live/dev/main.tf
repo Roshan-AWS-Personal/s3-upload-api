1dac3f855ee464a9b96025499bc005e7c61777ca
roshan-venkatesan-9563 <roshanvenkatesan9563@gmail.com>
2025-07-10 19:32:12 +1000

    added CORS for new bucket

--- live/dev/main.tf
+++ live/dev/main.tf
@@ -38,6 +38,18 @@ resource "aws_s3_bucket_cors_configuration" "upload_bucket_cors" {
   }
 }
 
+resource "aws_s3_bucket_cors_configuration" "documents_bucket_cors" {
+  bucket = aws_s3_bucket.documents_bucket.id
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "GET"]
+    allowed_origins = ["*"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+}
+
 
 resource "aws_s3_bucket_versioning" "upload_bucket_versioning" {
   bucket = aws_s3_bucket.image_upload_bucket.id
