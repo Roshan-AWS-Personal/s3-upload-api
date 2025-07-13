fcd290dcbf10ab165cfbac639e996587802cb108
roshan-venkatesan-9563 <roshanvenkatesan9563@gmail.com>
2025-07-13 11:55:35 +1000

    fixed another reference

--- live/dev/lambda.tf
+++ live/dev/lambda.tf
@@ -59,7 +59,7 @@ data "archive_file" "upload_lambda_zip" {
 }
 
 resource "aws_s3_bucket_notification" "images_trigger" {
-  bucket = aws_s3_bucket.images_bucket.id
+  bucket = aws_s3_bucket.image_upload_bucket.id
 
   lambda_function {
     lambda_function_arn = aws_lambda_function.s3_event_logger.arn
