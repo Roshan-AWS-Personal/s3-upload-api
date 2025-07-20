8940c6cd9c9972d3e88b1c7feab8b41b52a2be12
roshan-venkatesan-9563 <roshanvenkatesan9563@gmail.com>
2025-07-20 20:04:33 +1000

    a

--- live/prod/api_gateway.tf
+++ live/prod/api_gateway.tf
@@ -1,173 +1,173 @@
-resource "aws_api_gateway_rest_api" "upload_api" {
-  name               = "image-upload-api"
-  description        = "API Gateway for image uploads"
-  binary_media_types = ["multipart/form-data"]
-}
+# resource "aws_api_gateway_rest_api" "upload_api" {
+#   name               = "image-upload-api"
+#   description        = "API Gateway for image uploads"
+#   binary_media_types = ["multipart/form-data"]
+# }
 
-resource "aws_api_gateway_resource" "upload" {
-  rest_api_id = aws_api_gateway_rest_api.upload_api.id
-  parent_id   = aws_api_gateway_rest_api.upload_api.root_resource_id
-  path_part   = "upload"
-}
+# resource "aws_api_gateway_resource" "upload" {
+#   rest_api_id = aws_api_gateway_rest_api.upload_api.id
+#   parent_id   = aws_api_gateway_rest_api.upload_api.root_resource_id
+#   path_part   = "upload"
+# }
 
-resource "aws_api_gateway_authorizer" "cognito" {
-  name            = "upload-authorizer"
-  rest_api_id     = aws_api_gateway_rest_api.upload_api.id
-  identity_source = "method.request.header.Authorization"
-  type            = "COGNITO_USER_POOLS"
-  provider_arns   = [aws_cognito_user_pool.main.arn]
-}
+# resource "aws_api_gateway_authorizer" "cognito" {
+#   name            = "upload-authorizer"
+#   rest_api_id     = aws_api_gateway_rest_api.upload_api.id
+#   identity_source = "method.request.header.Authorization"
+#   type            = "COGNITO_USER_POOLS"
+#   provider_arns   = [aws_cognito_user_pool.main.arn]
+# }
 
-resource "aws_api_gateway_method" "get_upload_url_method" {
-  rest_api_id   = aws_api_gateway_rest_api.upload_api.id
-  resource_id   = aws_api_gateway_resource.upload.id
-  http_method   = "GET"
-  authorization = "COGNITO_USER_POOLS"
-  authorizer_id = aws_api_gateway_authorizer.cognito.id
-}
+# resource "aws_api_gateway_method" "get_upload_url_method" {
+#   rest_api_id   = aws_api_gateway_rest_api.upload_api.id
+#   resource_id   = aws_api_gateway_resource.upload.id
+#   http_method   = "GET"
+#   authorization = "COGNITO_USER_POOLS"
+#   authorizer_id = aws_api_gateway_authorizer.cognito.id
+# }
 
-resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.upload_api.id
-  resource_id             = aws_api_gateway_resource.upload.id
-  http_method             = aws_api_gateway_method.get_upload_url_method.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.image_uploader.invoke_arn
+# resource "aws_api_gateway_integration" "lambda_integration" {
+#   rest_api_id             = aws_api_gateway_rest_api.upload_api.id
+#   resource_id             = aws_api_gateway_resource.upload.id
+#   http_method             = aws_api_gateway_method.get_upload_url_method.http_method
+#   integration_http_method = "POST"
+#   type                    = "AWS_PROXY"
+#   uri                     = aws_lambda_function.image_uploader.invoke_arn
 
-  depends_on = [aws_lambda_function.image_uploader]
-}
+#   depends_on = [aws_lambda_function.image_uploader]
+# }
 
-resource "aws_lambda_permission" "api_gateway" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.image_uploader.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.upload_api.execution_arn}/*/*"
+# resource "aws_lambda_permission" "api_gateway" {
+#   statement_id  = "AllowAPIGatewayInvoke"
+#   action        = "lambda:InvokeFunction"
+#   function_name = aws_lambda_function.image_uploader.function_name
+#   principal     = "apigateway.amazonaws.com"
+#   source_arn    = "${aws_api_gateway_rest_api.upload_api.execution_arn}/*/*"
 
-  depends_on = [aws_lambda_function.image_uploader]
-}
+#   depends_on = [aws_lambda_function.image_uploader]
+# }
 
-resource "aws_api_gateway_method_response" "upload_get_response" {
-  rest_api_id = aws_api_gateway_rest_api.upload_api.id
-  resource_id = aws_api_gateway_resource.upload.id
-  http_method = aws_api_gateway_method.get_upload_url_method.http_method
-  status_code = "200"
+# resource "aws_api_gateway_method_response" "upload_get_response" {
+#   rest_api_id = aws_api_gateway_rest_api.upload_api.id
+#   resource_id = aws_api_gateway_resource.upload.id
+#   http_method = aws_api_gateway_method.get_upload_url_method.http_method
+#   status_code = "200"
 
-  response_models = {
-    "application/json" = "Empty"
-  }
+#   response_models = {
+#     "application/json" = "Empty"
+#   }
 
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin"  = true
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-  }
-}
+#   response_parameters = {
+#     "method.response.header.Access-Control-Allow-Origin"  = true
+#     "method.response.header.Access-Control-Allow-Headers" = true
+#     "method.response.header.Access-Control-Allow-Methods" = true
+#   }
+# }
 
-resource "aws_api_gateway_integration_response" "upload_get_response" {
-  rest_api_id = aws_api_gateway_rest_api.upload_api.id
-  resource_id = aws_api_gateway_resource.upload.id
-  http_method = aws_api_gateway_method.get_upload_url_method.http_method
-  status_code = "200"
+# resource "aws_api_gateway_integration_response" "upload_get_response" {
+#   rest_api_id = aws_api_gateway_rest_api.upload_api.id
+#   resource_id = aws_api_gateway_resource.upload.id
+#   http_method = aws_api_gateway_method.get_upload_url_method.http_method
+#   status_code = "200"
 
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
-    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET'"
-  }
+#   response_parameters = {
+#     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+#     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
+#     "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET'"
+#   }
 
-  depends_on = [
-    aws_api_gateway_integration.lambda_integration,
-    aws_api_gateway_method_response.upload_get_response
-  ]
-}
+#   depends_on = [
+#     aws_api_gateway_integration.lambda_integration,
+#     aws_api_gateway_method_response.upload_get_response
+#   ]
+# }
 
-resource "aws_api_gateway_method" "upload_options" {
-  rest_api_id   = aws_api_gateway_rest_api.upload_api.id
-  resource_id   = aws_api_gateway_resource.upload.id
-  http_method   = "OPTIONS"
-  authorization = "NONE"
-}
+# resource "aws_api_gateway_method" "upload_options" {
+#   rest_api_id   = aws_api_gateway_rest_api.upload_api.id
+#   resource_id   = aws_api_gateway_resource.upload.id
+#   http_method   = "OPTIONS"
+#   authorization = "NONE"
+# }
 
-resource "aws_api_gateway_integration" "upload_options_mock" {
-  rest_api_id = aws_api_gateway_rest_api.upload_api.id
-  resource_id = aws_api_gateway_resource.upload.id
-  http_method = aws_api_gateway_method.upload_options.http_method
-  type        = "MOCK"
+# resource "aws_api_gateway_integration" "upload_options_mock" {
+#   rest_api_id = aws_api_gateway_rest_api.upload_api.id
+#   resource_id = aws_api_gateway_resource.upload.id
+#   http_method = aws_api_gateway_method.upload_options.http_method
+#   type        = "MOCK"
 
-  request_templates = {
-    "application/json" = "{\"statusCode\": 200}"
-  }
-}
+#   request_templates = {
+#     "application/json" = "{\"statusCode\": 200}"
+#   }
+# }
 
-resource "aws_api_gateway_method_response" "upload_options_response" {
-  rest_api_id = aws_api_gateway_rest_api.upload_api.id
-  resource_id = aws_api_gateway_resource.upload.id
-  http_method = aws_api_gateway_method.upload_options.http_method
-  status_code = "200"
+# resource "aws_api_gateway_method_response" "upload_options_response" {
+#   rest_api_id = aws_api_gateway_rest_api.upload_api.id
+#   resource_id = aws_api_gateway_resource.upload.id
+#   http_method = aws_api_gateway_method.upload_options.http_method
+#   status_code = "200"
 
-  response_models = {
-    "application/json" = "Empty"
-  }
+#   response_models = {
+#     "application/json" = "Empty"
+#   }
 
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-    "method.response.header.Access-Control-Allow-Origin"  = true
-  }
-}
+#   response_parameters = {
+#     "method.response.header.Access-Control-Allow-Headers" = true
+#     "method.response.header.Access-Control-Allow-Methods" = true
+#     "method.response.header.Access-Control-Allow-Origin"  = true
+#   }
+# }
 
-resource "aws_api_gateway_integration_response" "upload_options_response" {
-  rest_api_id = aws_api_gateway_rest_api.upload_api.id
-  resource_id = aws_api_gateway_resource.upload.id
-  http_method = aws_api_gateway_method.upload_options.http_method
-  status_code = "200"
+# resource "aws_api_gateway_integration_response" "upload_options_response" {
+#   rest_api_id = aws_api_gateway_rest_api.upload_api.id
+#   resource_id = aws_api_gateway_resource.upload.id
+#   http_method = aws_api_gateway_method.upload_options.http_method
+#   status_code = "200"
 
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
-    "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET'"
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-  }
+#   response_parameters = {
+#     "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,Authorization'"
+#     "method.response.header.Access-Control-Allow-Methods" = "'OPTIONS,GET'"
+#     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+#   }
 
-  response_templates = {
-    "application/json" = ""
-  }
+#   response_templates = {
+#     "application/json" = ""
+#   }
 
-  depends_on = [
-    aws_api_gateway_integration.upload_options_mock,
-    aws_api_gateway_method_response.upload_options_response
-  ]
-}
+#   depends_on = [
+#     aws_api_gateway_integration.upload_options_mock,
+#     aws_api_gateway_method_response.upload_options_response
+#   ]
+# }
 
-resource "aws_api_gateway_deployment" "api_deployment" {
-  rest_api_id = aws_api_gateway_rest_api.upload_api.id
+# resource "aws_api_gateway_deployment" "api_deployment" {
+#   rest_api_id = aws_api_gateway_rest_api.upload_api.id
 
-  triggers = {
-    redeploy = sha1(jsonencode([
-      aws_api_gateway_resource.upload.id,
-      aws_api_gateway_method.get_upload_url_method.id,
-      aws_api_gateway_integration.lambda_integration.id
-    ]))
-  }
+#   triggers = {
+#     redeploy = sha1(jsonencode([
+#       aws_api_gateway_resource.upload.id,
+#       aws_api_gateway_method.get_upload_url_method.id,
+#       aws_api_gateway_integration.lambda_integration.id
+#     ]))
+#   }
 
-  lifecycle {
-    create_before_destroy = true
-  }
+#   lifecycle {
+#     create_before_destroy = true
+#   }
 
-  depends_on = [
-    aws_api_gateway_integration_response.upload_get_response,
-    aws_api_gateway_integration_response.upload_options_response
-  ]
-}
+#   depends_on = [
+#     aws_api_gateway_integration_response.upload_get_response,
+#     aws_api_gateway_integration_response.upload_options_response
+#   ]
+# }
 
-resource "aws_api_gateway_stage" "stage" {
-  stage_name    = var.stage_name
-  rest_api_id   = aws_api_gateway_rest_api.upload_api.id
-  deployment_id = aws_api_gateway_deployment.api_deployment.id
+# resource "aws_api_gateway_stage" "stage" {
+#   stage_name    = var.stage_name
+#   rest_api_id   = aws_api_gateway_rest_api.upload_api.id
+#   deployment_id = aws_api_gateway_deployment.api_deployment.id
 
-  depends_on = [aws_api_gateway_deployment.api_deployment]
-}
+#   depends_on = [aws_api_gateway_deployment.api_deployment]
+# }
 
-output "upload_api_url" {
-  value = "https://${aws_api_gateway_rest_api.upload_api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.stage.stage_name}/upload"
-}
+# output "upload_api_url" {
+#   value = "https://${aws_api_gateway_rest_api.upload_api.id}.execute-api.${var.aws_region}.amazonaws.com/${aws_api_gateway_stage.stage.stage_name}/upload"
+# }
--- live/prod/lambda.tf
+++ live/prod/lambda.tf
@@ -1,211 +1,211 @@
-# # Complete and corrected Terraform configuration for Lambda, IAM, and KMS
+# Complete and corrected Terraform configuration for Lambda, IAM, and KMS
 
-# resource "aws_iam_role" "s3_event_lambda_role" {
-#   name = "s3-event-lambda-role"
+resource "aws_iam_role" "s3_event_lambda_role" {
+  name = "s3-event-lambda-role"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [{
-#       Action = "sts:AssumeRole",
-#       Effect = "Allow",
-#       Principal = {
-#         Service = "lambda.amazonaws.com"
-#       }
-#     }]
-#   })
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# resource "aws_iam_role" "image_uploader_lambda_exec_role" {
-#   name = "lambda_s3_upload_role"
+resource "aws_iam_role" "image_uploader_lambda_exec_role" {
+  name = "lambda_s3_upload_role"
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [{
-#       Action = "sts:AssumeRole",
-#       Principal = {
-#         Service = "lambda.amazonaws.com"
-#       },
-#       Effect = "Allow",
-#       Sid    = ""
-#     }]
-#   })
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow",
+      Sid    = ""
+    }]
+  })
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# data "archive_file" "s3_event_lambda_zip" {
-#   type        = "zip"
-#   source_dir  = "${path.module}/lambda/event_logger"
-#   output_path = "${path.module}/zips/s3_event_logger.zip"
-# }
+data "archive_file" "s3_event_lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda/event_logger"
+  output_path = "${path.module}/zips/s3_event_logger.zip"
+}
 
-# data "archive_file" "upload_lambda_zip" {
-#   type        = "zip"
-#   source_dir  = "${path.module}/lambda/upload_handler"
-#   output_path = "${path.module}/zips/upload_handler.zip"
-# }
+data "archive_file" "upload_lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/lambda/upload_handler"
+  output_path = "${path.module}/zips/upload_handler.zip"
+}
 
-# data "aws_caller_identity" "current" {}
+data "aws_caller_identity" "current" {}
 
-# resource "aws_kms_key" "lambda_key" {
-#   description         = "KMS key for encrypting Lambda environment variables"
-#   enable_key_rotation = false
+resource "aws_kms_key" "lambda_key" {
+  description         = "KMS key for encrypting Lambda environment variables"
+  enable_key_rotation = false
 
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Id      = "key-lambda-upload",
-#     Statement: [
-#       {
-#         Sid: "AllowLambdaToDecrypt",
-#         Effect: "Allow",
-#         Principal: {
-#           AWS: aws_iam_role.image_uploader_lambda_exec_role.arn
-#         },
-#         Action: [
-#           "kms:Decrypt",
-#           "kms:GenerateDataKey"
-#         ],
-#         Resource: "*"
-#       },
-#       {
-#         Sid: "AllowRootAccountFullAccess",
-#         Effect: "Allow",
-#         Principal: {
-#           AWS: "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-#         },
-#         Action: "kms:*",
-#         Resource: "*"
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Id      = "key-lambda-upload",
+    Statement: [
+      {
+        Sid: "AllowLambdaToDecrypt",
+        Effect: "Allow",
+        Principal: {
+          AWS: aws_iam_role.image_uploader_lambda_exec_role.arn
+        },
+        Action: [
+          "kms:Decrypt",
+          "kms:GenerateDataKey"
+        ],
+        Resource: "*"
+      },
+      {
+        Sid: "AllowRootAccountFullAccess",
+        Effect: "Allow",
+        Principal: {
+          AWS: "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        },
+        Action: "kms:*",
+        Resource: "*"
+      }
+    ]
+  })
+}
 
-# resource "aws_lambda_function" "s3_event_logger" {
-#   function_name    = "s3-event-logger"
-#   handler          = "event_logger.lambda_handler"
-#   runtime          = "python3.11"
-#   role             = aws_iam_role.s3_event_lambda_role.arn
-#   filename         = data.archive_file.s3_event_lambda_zip.output_path
-#   source_code_hash = data.archive_file.s3_event_lambda_zip.output_base64sha256
-#   timeout          = 10
+resource "aws_lambda_function" "s3_event_logger" {
+  function_name    = "s3-event-logger"
+  handler          = "event_logger.lambda_handler"
+  runtime          = "python3.11"
+  role             = aws_iam_role.s3_event_lambda_role.arn
+  filename         = data.archive_file.s3_event_lambda_zip.output_path
+  source_code_hash = data.archive_file.s3_event_lambda_zip.output_base64sha256
+  timeout          = 10
 
-#   environment {
-#     variables = {
-#       DYNAMODB_TABLE = var.dynamodb_table
-#     }
-#   }
+  environment {
+    variables = {
+      DYNAMODB_TABLE = var.dynamodb_table
+    }
+  }
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# resource "aws_lambda_function" "image_uploader" {
-#   function_name    = "image-uploader-dev"
-#   kms_key_arn      = aws_kms_key.lambda_key.arn
-#   filename         = data.archive_file.upload_lambda_zip.output_path
-#   source_code_hash = data.archive_file.upload_lambda_zip.output_base64sha256
-#   role             = aws_iam_role.image_uploader_lambda_exec_role.arn
-#   handler          = "upload_handler.lambda_handler"
-#   runtime          = "python3.11"
+resource "aws_lambda_function" "image_uploader" {
+  function_name    = "image-uploader-dev"
+  kms_key_arn      = aws_kms_key.lambda_key.arn
+  filename         = data.archive_file.upload_lambda_zip.output_path
+  source_code_hash = data.archive_file.upload_lambda_zip.output_base64sha256
+  role             = aws_iam_role.image_uploader_lambda_exec_role.arn
+  handler          = "upload_handler.lambda_handler"
+  runtime          = "python3.11"
 
-#   environment {
-#     variables = {
-#       IMAGES_BUCKET     = aws_s3_bucket.image_upload_bucket.bucket
-#       DOCUMENTS_BUCKET  = aws_s3_bucket.documents_bucket.bucket
-#       UPLOAD_API_SECRET = var.upload_api_secret
-#     }
-#   }
+  environment {
+    variables = {
+      IMAGES_BUCKET     = aws_s3_bucket.image_upload_bucket.bucket
+      DOCUMENTS_BUCKET  = aws_s3_bucket.documents_bucket.bucket
+      UPLOAD_API_SECRET = var.upload_api_secret
+    }
+  }
 
-#   depends_on = [
-#     aws_iam_role_policy.lambda_s3_policy,
-#     aws_kms_key.lambda_key
-#   ]
+  depends_on = [
+    aws_iam_role_policy.lambda_s3_policy,
+    aws_kms_key.lambda_key
+  ]
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# resource "aws_iam_role_policy" "lambda_s3_policy" {
-#   name = "lambda-s3-upload-policy"
-#   role = aws_iam_role.image_uploader_lambda_exec_role.id
+resource "aws_iam_role_policy" "lambda_s3_policy" {
+  name = "lambda-s3-upload-policy"
+  role = aws_iam_role.image_uploader_lambda_exec_role.id
 
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Effect = "Allow",
-#         Action = ["s3:PutObject"],
-#         Resource = [
-#           "${aws_s3_bucket.image_upload_bucket.arn}/*",
-#           "${aws_s3_bucket.documents_bucket.arn}/*"
-#         ]
-#       },
-#       {
-#         Effect = "Allow",
-#         Action = [
-#           "logs:CreateLogGroup",
-#           "logs:CreateLogStream",
-#           "logs:PutLogEvents"
-#         ],
-#         Resource = "*"
-#       },
-#       {
-#         Effect = "Allow",
-#         Action = ["kms:Decrypt"],
-#         Resource = aws_kms_key.lambda_key.arn
-#       }
-#     ]
-#   })
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = ["s3:PutObject"],
+        Resource = [
+          "${aws_s3_bucket.image_upload_bucket.arn}/*",
+          "${aws_s3_bucket.documents_bucket.arn}/*"
+        ]
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = ["kms:Decrypt"],
+        Resource = aws_kms_key.lambda_key.arn
+      }
+    ]
+  })
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# resource "aws_iam_role_policy" "lambda_ses_policy" {
-#   name = "lambda-ses-send-policy"
-#   role = aws_iam_role.image_uploader_lambda_exec_role.id
+resource "aws_iam_role_policy" "lambda_ses_policy" {
+  name = "lambda-ses-send-policy"
+  role = aws_iam_role.image_uploader_lambda_exec_role.id
 
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Effect = "Allow",
-#         Action = ["ses:SendEmail", "ses:SendRawEmail"],
-#         Resource = "*"
-#       }
-#     ]
-#   })
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = ["ses:SendEmail", "ses:SendRawEmail"],
+        Resource = "*"
+      }
+    ]
+  })
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
-# resource "aws_iam_role_policy" "dynamodb_write_policy" {
-#   name = "LambdaDynamoDBWritePolicy"
-#   role = aws_iam_role.image_uploader_lambda_exec_role.name
+resource "aws_iam_role_policy" "dynamodb_write_policy" {
+  name = "LambdaDynamoDBWritePolicy"
+  role = aws_iam_role.image_uploader_lambda_exec_role.name
 
-#   policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [
-#       {
-#         Action = ["dynamodb:PutItem"],
-#         Effect = "Allow",
-#         Resource = aws_dynamodb_table.file_upload_metadata.arn
-#       }
-#     ]
-#   })
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = ["dynamodb:PutItem"],
+        Effect = "Allow",
+        Resource = aws_dynamodb_table.file_upload_metadata.arn
+      }
+    ]
+  })
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
