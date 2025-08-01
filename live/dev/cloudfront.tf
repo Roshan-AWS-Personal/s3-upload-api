ec724a152e679a0448dd7fd19ba6622769ef4f78
roshan-venkatesan-9563 <roshanvenkatesan9563@gmail.com>
2025-08-01 20:13:46 +1000

    please

--- live/dev/cloudfront.tf
+++ live/dev/cloudfront.tf
@@ -6,6 +6,25 @@ resource "aws_cloudfront_origin_access_control" "s3_oac" {
   description                       = "OAC for S3 frontend site"
 }
 
+resource "aws_cloudfront_origin_request_policy" "forward_auth" {
+  name = "forward-auth-header"
+
+  headers_config {
+    header_behavior = "whitelist"
+    headers {
+      items = ["Authorization", "Origin"]
+    }
+  }
+
+  cookies_config {
+    cookie_behavior = "none"
+  }
+
+  query_strings_config {
+    query_string_behavior = "all"
+  }
+}
+
 resource "aws_cloudfront_distribution" "frontend" {
   enabled             = true
   default_root_object = "index.html"
@@ -15,6 +34,18 @@ resource "aws_cloudfront_distribution" "frontend" {
     origin_id   = "s3-upload-site"
 
     origin_access_control_id = aws_cloudfront_origin_access_control.s3_oac.id
+  }
+
+  origin {
+    domain_name = "${aws_api_gateway_rest_api.upload_api.id}.execute-api.${var.aws_region}.amazonaws.com"
+    origin_id   = "api-upload"
+
+    custom_origin_config {
+      origin_protocol_policy = "https-only"
+      http_port              = 80
+      https_port             = 443
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
   }
 
   default_cache_behavior {
@@ -30,6 +61,30 @@ resource "aws_cloudfront_distribution" "frontend" {
         forward = "none"
       }
     }
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "upload*"
+    allowed_methods  = ["GET", "OPTIONS", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "api-upload"
+
+    viewer_protocol_policy     = "redirect-to-https"
+    cache_policy_id            = "Managed-CachingDisabled"
+    origin_request_policy_id   = aws_cloudfront_origin_request_policy.forward_auth.id
+    compress                   = true
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "files*"
+    allowed_methods  = ["GET", "OPTIONS", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "api-upload"
+
+    viewer_protocol_policy     = "redirect-to-https"
+    cache_policy_id            = "Managed-CachingDisabled"
+    origin_request_policy_id   = aws_cloudfront_origin_request_policy.forward_auth.id
+    compress                   = true
   }
 
   restrictions {
