6f6063a21d2c831852402050e053ae73eec4708e
roshan-venkatesan-9563 <roshanvenkatesan9563@gmail.com>
2025-08-02 21:05:03 +1000

    cv

--- live/dev/dynamodb.tf
+++ live/dev/dynamodb.tf
@@ -1,11 +1,25 @@
 resource "aws_dynamodb_table" "file_upload_metadata" {
   name         = "file_upload_metadata"
-  billing_mode = "PAY_PER_REQUEST"  # On-demand, no need to manage throughput
+  billing_mode = "PAY_PER_REQUEST"
   hash_key     = "upload_id"
 
   attribute {
     name = "upload_id"
     type = "S"
+  }
+
+  # Add the attribute you plan to query on
+  attribute {
+    name = "username"
+    type = "S"
+  }
+
+  # Define the GSI so you can KeyConditionExpression on username
+  global_secondary_index {
+    name               = "username-index"
+    hash_key           = "username"
+    projection_type    = "ALL"
+    # No need for read/write capacity under PAY_PER_REQUEST
   }
 
   tags = {
