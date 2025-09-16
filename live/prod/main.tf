aa63b7ef2f029beda6d6481b1448632d2c35a619
roshan-venkatesan-9563 <roshanvenkatesan9563@gmail.com>
2025-09-16 20:53:09 +1000

    added prod variable back to cognito and reverted bucket variable

--- live/prod/main.tf
+++ live/prod/main.tf
@@ -1,27 +1,15 @@
 terraform {
   required_providers {
-    aws = { source = "hashicorp/aws" }
-    docker = { source = "kreuzwerker/docker" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.35" # or 5.30, 5.27, etc.
+    }
   }
-}
 
-provider "aws" {}
-
-data "aws_ecr_authorization_token" "ecr" {}
-
-locals {
-  ecr_address = replace(data.aws_ecr_authorization_token.ecr.proxy_endpoint, "https://", "")
-}
-
-provider "docker" {
-  registry_auth {
-    address  = local.ecr_address
-    username = data.aws_ecr_authorization_token.ecr.user_name
-    password = data.aws_ecr_authorization_token.ecr.password
-  }
+  required_version = ">= 1.5.0"
 }
 resource "aws_s3_bucket" "image_upload_bucket" {
-  bucket = "s3-image-upload-api-2712"
+  bucket = "s3-image-upload-api-2712-${var.env}"
   force_destroy = true
 
   tags = {
@@ -30,7 +18,7 @@ resource "aws_s3_bucket" "image_upload_bucket" {
 }
 
 resource "aws_s3_bucket" "documents_bucket" {
-  bucket = "s3-upload-documents"
+  bucket = "s3-upload-documents-${var.env}"
   force_destroy = true
     tags = {
     Name = "document-upload-api-bucket"
@@ -69,42 +57,3 @@ resource "aws_s3_bucket_versioning" "upload_bucket_versioning" {
   }
 }
 
-data "aws_iam_policy_document" "s3_to_sqs" {
-  statement {
-    effect = "Allow"
-
-    principals {
-      type        = "Service"
-      identifiers = ["s3.amazonaws.com"]
-    }
-
-    actions   = ["sqs:SendMessage"]
-    resources = [aws_sqs_queue.ingest_queue.arn]
-
-    condition {
-      test     = "ArnEquals"
-      variable = "aws:SourceArn"
-      values   = [aws_s3_bucket.documents_bucket.arn]
-    }
-  }
-}
-
-resource "aws_sqs_queue_policy" "allow_s3" {
-  queue_url = aws_sqs_queue.ingest_queue.id
-  policy    = data.aws_iam_policy_document.s3_to_sqs.json
-}
-
-resource "aws_s3_bucket_notification" "documents_notifications" {
-  bucket = aws_s3_bucket.documents_bucket.id
-
-  # S3 -> SQS (for ingest pipeline)
-  queue {
-    queue_arn     = aws_sqs_queue.ingest_queue.arn
-    # be explicit; avoid wildcard overlap with other rules
-    events        = ["s3:ObjectCreated:Put", "s3:ObjectCreated:CompleteMultipartUpload", "s3:ObjectCreated:Copy"]
-    filter_prefix = "docs/"
-  }
-
-  # only the SQS rule here; no lambda rule on this bucket
-  depends_on = [aws_sqs_queue_policy.allow_s3]
-}
